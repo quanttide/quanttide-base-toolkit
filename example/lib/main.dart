5c5437ee9254e85098df2b26880d0201abf55fde
Guo Zhang <zhangguo@quanttide.com>
2022-07-25 03:24:55 +0800

    feature: 增加NetworkFutureBuilder组件

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:quanttide_design/quanttide_design.dart';
 
+import 'utils.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '量潮设计语言',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -26,7 +28,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: '量潮设计语言'),
     );
   }
 }
@@ -50,36 +52,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: NetworkFutureBuilder(
-        future: Future.delayed(const Duration(seconds: 3)),
-        withData: (data) => const Text('加载成功'),
+        future: errorRequest(),
+        // Future.delayed(const Duration(seconds: 2)),
+        withData: (data) => const Center(child: Text('加载成功')),
       ),
     );
   }
--- lib/src/widgets/network.dart
+++ lib/src/widgets/network.dart
@@ -1,6 +1,13 @@
+/// 网络加载组件
+///
+/// 参考资料：
+///   - https://blog.csdn.net/u011272795/article/details/83010974
+
+import 'package:flutter/foundation.dart';
 import "package:flutter/material.dart";
 
 
+/// 网络加载FutureBuilder组件
 class NetworkFutureBuilder extends StatefulWidget {
   final Future future;
   final Function withData;
@@ -12,15 +19,14 @@ class NetworkFutureBuilder extends StatefulWidget {
     Key? key,
     required this.future,
     required this.withData,
-    this.nothing,
-    this.waiting=waitingFunction,
-    this.withError,
+    this.nothing=defaultNothing,
+    this.waiting=defaultWaiting,
+    this.withError=defaultWithError,
   }) : super(key: key);
 
   @override
   NetworkFutureBuilderState createState() => NetworkFutureBuilderState();
 }
-
 
 class NetworkFutureBuilderState extends State<NetworkFutureBuilder> {
   @override
@@ -30,12 +36,12 @@ class NetworkFutureBuilderState extends State<NetworkFutureBuilder> {
       builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.none:
-            return const Text('未加载');
+            return widget.nothing!();
           case ConnectionState.waiting:
             return widget.waiting!();
           default:
             if (snapshot.hasError) {
-              return const Text('加载失败');
+              return widget.withError!(snapshot.error);
             } else {
               return widget.withData(snapshot.data);
             }
@@ -46,8 +52,34 @@ class NetworkFutureBuilderState extends State<NetworkFutureBuilder> {
 }
 
 
-Widget waitingFunction(){
+/// 默认"未加载"
+Widget defaultNothing() {
   return const Center(
-    child: Text('加载中')
+    child: Text('未加载')
   );
+}
+
+/// 默认"加载中"
+Widget defaultWaiting() {
+  return const Center(
+      child: CircularProgressIndicator()
+  );
+}
+
+/// 默认"加载错误"
+Widget defaultWithError(dynamic error) {
+  // `kDebugMode` is better by [`kReleaseMode`'s doc](https://api.flutter.dev/flutter/foundation/kReleaseMode-constant.html)
+  // > Generally it is better to use kDebugMode or assert to gate code,
+  // > since using kReleaseMode will introduce differences between release and profile builds,
+  // > which makes performance testing less representative.
+  if (kDebugMode) {
+    return Center(
+      child: ErrorWidget(error)
+    );
+  }
+  else {
+    return const Center(
+      child: Text('加载失败')
+    );
+  }
 }
