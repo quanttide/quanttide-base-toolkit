9d8405d4b3c7614ed33712dca8e5c19843c3d813
Guo Zhang <zhangguo@quanttide.com>
2022-08-29 02:02:04 +0800

    feature: recase改造为泛型，单元测试通过。

--- lib/src/utils/recase.dart
+++ lib/src/utils/recase.dart
@@ -6,19 +6,16 @@ dynamic convertKeysToCamel<T>(T data){
   if (data is Map<String, dynamic>){
     // https://api.dart.dev/stable/2.15.1/dart-core/Map/map.html
     return data.map((key, value){
-      if (value is Map<String, dynamic>){
-        // 对字典做递归
+      if (value is Map<String, dynamic> || value is List<Map<String, dynamic>>){
+        // 对字典或列表做递归
         value = convertKeysToCamel(value);
-      }
-      else if (value is List<Map<String, dynamic>>){
-        // 对列表的每个元素做递归
-        value = value.map((item) => convertKeysToCamel(item)).toList();
       }
       // https://pub.dev/packages/recase
       return MapEntry(key.camelCase, value);
     });
   }
   else if (data is List<Map<String, dynamic>>){
+    // 对列表里的每个元素做递归
     return data.map((item) => convertKeysToCamel(item)).toList();
   }
   else {
