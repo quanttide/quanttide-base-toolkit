9d3c459b7f87158224ff821af67c78eee05d175a
Guo Zhang <zhangguo@quanttide.com>
2023-07-04 22:02:05 +0800

    feat: 增加IDField的单元测试

--- django_quanttide/models/fields.py
+++ django_quanttide/models/fields.py
@@ -12,8 +12,20 @@ class IDField(models.UUIDField):
     ID字段
 
     默认作为主键使用；
-    不作为主键使用时，设置`primary_key=False`，并建议自定义`verbose_name`。
+    不作为主键、作为关联ID使用时，设置`primary_key=False`，
+    同时建议自定义`verbose_name`，格式为"<关联模型名称>ID"。
+
+    :param primary_key: 是否作为主键，默认为True
+    :type primary_key: bool
+    :param editable: 是否可编辑，默认为非主键字段可编辑
+    :type editable: bool
+    :param default: 默认值，默认为uuid.uuid4
+    :type default: callable
+    :param verbose_name: 字段名称，默认为'ID'
+    :type verbose_name: str
     """
+    description = 'ID字段'
+
     def __init__(self, **options):
         options['primary_key'] = options.get('primary_key', True)
         options['editable'] = options.get('editable', not options['primary_key'])
--- tests/test_models_fields.py
+++ tests/test_models_fields.py
@@ -1,9 +1,19 @@
+import uuid
 from django.test import TestCase
-
-from .models import ExampleModel
+from django_quanttide.models import IDField
 
 
 class IDFieldTestCase(TestCase):
-    def test_init(self):
-        model_instance = ExampleModel()
-        self.assertTrue(model_instance.id)
+    def test_defaults(self):
+        field = IDField()
+        self.assertTrue(field.primary_key)
+        self.assertFalse(field.editable)
+        self.assertEqual(field.verbose_name, 'ID')
+        self.assertIsInstance(field.default(), uuid.UUID)
+
+    def test_custom_options(self):
+        field = IDField(primary_key=False, verbose_name='Custom ID')
+        self.assertFalse(field.primary_key)
+        self.assertTrue(field.editable)
+        self.assertEqual(field.verbose_name, 'Custom ID')
+        self.assertIsInstance(field.default(), uuid.UUID)
